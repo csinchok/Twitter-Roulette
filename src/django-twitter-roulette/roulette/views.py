6e767cbb91bfcca78c58d1829e800a195522d2db
Chris Sinchok <chris@sinchok.com>
2011-09-01 20:40:00 -0400

    Tweepy stuff

--- src/django-twitter-roulette/roulette/views.py
+++ src/django-twitter-roulette/roulette/views.py
@@ -13,15 +13,16 @@ def logout_view(request, template_name='roulette/logout.html'):
 def home(request, template_name='roulette/home.html'):
     this_round = Round.objects.filter(round_end__gte=datetime.datetime.now())[0]
     bullets = Bullet.objects.filter(roulette_round=this_round).order_by("-date_submitted")[:10]
-    for bullet in bullets:
-        votes = Vote.objects.filter(bullet=bullet, user=request.user)
-        if votes.count() == 1:
-            if votes[0].value == 1:
-                setattr(bullet, 'voted', 'up')
+    if request.user.is_authenticated():
+        for bullet in bullets:
+            votes = Vote.objects.filter(bullet=bullet, user=request.user)
+            if votes.count() == 1:
+                if votes[0].value == 1:
+                    setattr(bullet, 'voted', 'up')
+                else:
+                    setattr(bullet, 'voted', 'down')
             else:
-                setattr(bullet, 'voted', 'down')
-        else:
-            setattr(bullet, 'voted', 'undef')
+                setattr(bullet, 'voted', 'undef')
     return render_to_response(template_name, {'ishome': True, 'bullets': bullets, 'this_round': this_round}, context_instance=RequestContext(request))
     
 def login_error_view(request,template_name='roulette/login-error.html'):
